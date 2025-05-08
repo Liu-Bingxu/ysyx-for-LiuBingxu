f5813ac06714c80ae54f3da219454a52a1940a7f
Liu Bingxu <qwe15889844242@163.com>
2025-05-08 21:43:44 +0800

    add the gtx_clk = rx_clk in no ddr platform

--- rtl/periph/enet/rtl/enet_rgmii_to_gmii_dummy.v
+++ rtl/periph/enet/rtl/enet_rgmii_to_gmii_dummy.v
@@ -1,24 +1,24 @@
 module enet_rgmii_to_gmii_dummy#(
-    //输入数据IO延时(如果为n,表示延时n*78ps) 
+    //锟斤拷锟斤拷锟斤拷锟斤拷IO锟斤拷时(锟斤拷锟轿猲,锟斤拷示锟斤拷时n*78ps) 
     parameter IDELAY_VALUE = 0
 )(
-    input              idelay_clk  , //IDELAY时钟
-    //以太网GMII接口
-    output             gmii_rx_clk , //GMII接收时钟
-    output             gmii_rx_dv  , //GMII接收数据有效信号
-    output             gmii_rx_er  , //GMII接收数据错误信号
-    output      [7:0]  gmii_rxd    , //GMII接收数据
-    output             gmii_tx_clk , //GMII发送时钟
-    input              gmii_tx_en  , //GMII发送数据使能信号
-    input              gmii_tx_er  , //GMII发送数据错误信号
-    input       [7:0]  gmii_txd    , //GMII发送数据            
-    //以太网RGMII接口   
-    input              rgmii_rxc   , //RGMII接收时钟
-    input              rgmii_rx_ctl, //RGMII接收数据控制信号
-    input       [3:0]  rgmii_rxd   , //RGMII接收数据
-    output             rgmii_txc   , //RGMII发送时钟    
-    output             rgmii_tx_ctl, //RGMII发送数据控制信号
-    output      [3:0]  rgmii_txd     //RGMII发送数据          
+    input              idelay_clk  , //IDELAY时锟斤拷
+    //锟斤拷太锟斤拷GMII锟接匡拷
+    output             gmii_rx_clk , //GMII锟斤拷锟斤拷时锟斤拷
+    output             gmii_rx_dv  , //GMII锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷效锟脚猴拷
+    output             gmii_rx_er  , //GMII锟斤拷锟斤拷锟斤拷锟捷达拷锟斤拷锟脚猴拷
+    output      [7:0]  gmii_rxd    , //GMII锟斤拷锟斤拷锟斤拷锟斤拷
+    output             gmii_tx_clk , //GMII锟斤拷锟斤拷时锟斤拷
+    input              gmii_tx_en  , //GMII锟斤拷锟斤拷锟斤拷锟斤拷使锟斤拷锟脚猴拷
+    input              gmii_tx_er  , //GMII锟斤拷锟斤拷锟斤拷锟捷达拷锟斤拷锟脚猴拷
+    input       [7:0]  gmii_txd    , //GMII锟斤拷锟斤拷锟斤拷锟斤拷            
+    //锟斤拷太锟斤拷RGMII锟接匡拷   
+    input              rgmii_rxc   , //RGMII锟斤拷锟斤拷时锟斤拷
+    input              rgmii_rx_ctl, //RGMII锟斤拷锟斤拷锟斤拷锟捷匡拷锟斤拷锟脚猴拷
+    input       [3:0]  rgmii_rxd   , //RGMII锟斤拷锟斤拷锟斤拷锟斤拷
+    output             rgmii_txc   , //RGMII锟斤拷锟斤拷时锟斤拷    
+    output             rgmii_tx_ctl, //RGMII锟斤拷锟斤拷锟斤拷锟捷匡拷锟斤拷锟脚猴拷
+    output      [3:0]  rgmii_txd     //RGMII锟斤拷锟斤拷锟斤拷锟斤拷          
 );
 
 assign gmii_rx_clk  = 1'b0; 
@@ -27,7 +27,7 @@ assign gmii_rx_er   = 1'b0;
 assign gmii_rxd     = 8'b0; 
 assign gmii_tx_clk  = 1'b0; 
 
-assign rgmii_txc    = 1'b0;
+assign rgmii_txc    = rgmii_rxc;
 assign rgmii_tx_ctl = 1'b0;
 assign rgmii_txd    = 4'b0;
 
